b8e840cbb3a3df2ef3753d513387953406bd07a9
Bartosz Maciejewicz <bartek@clearmedia.pl>
2023-02-21 10:21:30 +0100

    feat(cirlcle): remove import of not used commands

--- app/Services/ExternalServices/CircleSo/CircleSo.php
+++ app/Services/ExternalServices/CircleSo/CircleSo.php
@@ -7,14 +7,9 @@ use App\Services\ExternalServices\CircleSo\Client\CircleSoClient;
 use App\Services\ExternalServices\CircleSo\Commands\Accounts\CircleSoCommandGetCommunityList;
 use App\Services\ExternalServices\CircleSo\Commands\Members\CircleSoCommandInviteMember;
 use App\Services\ExternalServices\CircleSo\Commands\Members\CircleSoCommandRemoveMemberFromCommunity;
-use App\Services\ExternalServices\CircleSo\Commands\Members\CircleSoCommandSearchMember;
 use App\Services\ExternalServices\CircleSo\Commands\SpaceGroups\CircleSoCommandAddMemberToSpaceGroup;
-use App\Services\ExternalServices\CircleSo\Commands\SpaceGroups\CircleSoCommandGetMemberFromSpaceGroup;
-use App\Services\ExternalServices\CircleSo\Commands\SpaceGroups\CircleSoCommandGetSpaceGroups;
 use App\Services\ExternalServices\CircleSo\Commands\SpaceGroups\CircleSoCommandRemoveMemberFromSpaceGroups;
 use App\Services\ExternalServices\CircleSo\Commands\Spaces\CircleSoCommandAddMemberToSpace;
-use App\Services\ExternalServices\CircleSo\Commands\Spaces\CircleSoCommandGetMemberFromSpace;
-use App\Services\ExternalServices\CircleSo\Commands\Spaces\CircleSoCommandGetSpaces;
 use App\Services\ExternalServices\CircleSo\Commands\Spaces\CircleSoCommandRemoveMemberFromSpace;
 
 class CircleSo extends AbstractCommandLoader
@@ -25,14 +20,9 @@ class CircleSo extends AbstractCommandLoader
         CircleSoCommandGetCommunityList::class => 'getCommunityList',
         CircleSoCommandInviteMember::class => 'inviteMember',
         CircleSoCommandRemoveMemberFromCommunity::class => 'removeMemberFromCommunity',
-        CircleSoCommandSearchMember::class => 'searchMember',
         CircleSoCommandAddMemberToSpaceGroup::class => 'addMemberToSpaceGroup',
-        CircleSoCommandGetMemberFromSpaceGroup::class => 'getMemberFromSpaceGroup',
-        CircleSoCommandGetSpaceGroups::class => 'getSpaceGroups',
         CircleSoCommandRemoveMemberFromSpaceGroups::class => 'removeMemberFromSpaceGroups',
         CircleSoCommandAddMemberToSpace::class => 'addMemberToSpace',
-        CircleSoCommandGetMemberFromSpace::class => 'getMemberFromSpace',
-        CircleSoCommandGetSpaces::class => 'getSpaces',
         CircleSoCommandRemoveMemberFromSpace::class => 'removeMemberFromSpace',
     ];
 
